9789e17e116c0650993f38257a09dccd517e3348
Tiago Cruz <tcruz@groupnet.io>
2018-07-12 15:11:19 +0200

    Update HTML and JS

--- html/js-index.js
+++ html/js-index.js
@@ -1,16 +1,24 @@
-var map = {"config":"","potencia":"W","amperagem":"A","voltagem":"V","temp":"\u00BAC","contador":""};
+var map = {
+    "Config": "",
+    "Potencia": "W",
+    "Amperagem": "A",
+    "Voltagem": "V",
+    "Temperatura": "\u00BAC",
+    "Contador": ""
+};
+
 function loadConfig() {
     var someUrl = "/config";
     $.ajax({
         url: someUrl,
         contentType: "application/json; charset=utf-8",
         dataType: "json",
-        success: function(response) {
+        success: function (response) {
             console.log(response);
-
+            $('input[name="hostname"]').val(response[0].hostname);
             $('input[name="nodeId"]').val(response[0].nodeId);
-            $('input[name="directionCurrentDetection"]').prop("checked",response[0].directionCurrentDetection);
-            $('select[name="notificationInterval"] option[value="'+response[0].notificationInterval+'"]').attr("selected","selected");
+            $('input[name="directionCurrentDetection"]').prop("checked", response[0].directionCurrentDetection);
+            $('select[name="notificationInterval"] option[value="' + response[0].notificationInterval + '"]').attr("selected", "selected");
 
             $('input[name="emoncmsApiKey"]').val(response[0].emoncmsApiKey);
             $('input[name="emoncmsUrl"]').val(response[0].emoncmsUrl);
@@ -24,32 +32,28 @@ function loadConfig() {
             $('input[name="wifiSecret"]').val(response[0].wifiSecret);
 
             $("#firmwareVersion").text(response[1].firmwareVersion);
-
         },
         timeout: 2000
-    })
+    });
 }
+
 function loadReadings() {
     var someUrl = "/readings";
     $.ajax({
         url: someUrl,
         contentType: "application/json; charset=utf-8",
         dataType: "json",
-        success: function(response) {
-
+        success: function (response) {
             $('#sensors').empty();
             Object.keys(response).forEach(function (key) {
-                $('#sensors').append('<li  class="list-group-item"><h6>'+key+': <span id="'+key+'" class="badge badge-secondary">'+response[key]+' '+map[key.split("_")[0]]+'</span></h6></li>');
-            })
-
-
+                $('#sensors').append('<li  class="list-group-item"><h6>' + key + ': <span id="' + key + '" class="badge badge-secondary">' + response[key] + ' ' + map[key.split("_")[0]] + '</span></h6></li>');
+            });
         },
         timeout: 2000
-    })
+    });
 }
 
-
-$(document).ready(function(){
+$(document).ready(function () {
     loadConfig();
     loadReadings();
     setInterval(loadReadings, 3000);
