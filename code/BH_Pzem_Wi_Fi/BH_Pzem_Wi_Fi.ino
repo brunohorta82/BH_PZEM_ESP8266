40c5dc47e90eafae5cc5ef03ccd2c18c35dd8f19
Bruno Horta <bh@void.pt>
2018-07-13 17:28:26 +0100

    optimização no codigo de condiguração

--- code/BH_Pzem_Wi_Fi/BH_Pzem_Wi_Fi.ino
+++ code/BH_Pzem_Wi_Fi/BH_Pzem_Wi_Fi.ino
@@ -19,14 +19,14 @@ String devAddrNames[15];  // array of (up to) 15 temperature sensors
 int sensorsCount = 0;
 
 void setup() {
-  
   Serial.begin(115200);
-  jw.setHostname(String(HOSTNAME).c_str());
+  loadStoredConfiguration();
+  jw.setHostname(hostname.c_str());
   jw.subscribe(infoCallback);
   jw.enableAP(false);
   jw.enableAPFallback(true);
   jw.enableSTA(true);
-  loadConfiguration();
+
   jw.addNetwork(wifiSSID.c_str(), wifiSecret.c_str());
 
   timerRead.begin(0);
@@ -61,9 +61,13 @@ void loop() {
     ESP.restart();
     return;
   }
-      
+    
       jw.loop();
-      loadNewConfig();
+      checkConfigChanges();
+      if(restartMqtt){
+        restartMqtt = false;
+        setupMQTT() ;
+       }
       float t = 0;
 
       if (timerRead.onTimeout(notificationInterval)){
