1cade555675154552b46dc875fc8e8d41ec3d635
André Rosa <andreros@gmail.com>
2018-07-17 16:24:26 +0100

    Cleanup markup dev files

--- html/index_andre.js
+++ html/index_andre.js
@@ -16,11 +16,14 @@ var limits = {"config": "0", "potencia": "2700", "amperagem": "32", "voltagem": 
 
 function loadConfig() {
     var someUrl = config.baseUrl + "/config";
-    $.ajax({
+    /*$.ajax({
         url: someUrl,
         contentType: "text/plain; charset=utf-8",
         dataType: "json",
-        success: function (response) {
+        success: function (response) {*/
+            var response = [{"nodeId":"mynode","notificationInterval":5,"directionCurrentDetection":0,"emoncmsApiKey": "","emoncmsPrefix": "","emoncmsUrl": "","mqttIpDns": "192.168.187.227","mqttUsername": "","mqttPassword": "","wifiSSID": "MAKER_IOT","wifiSecret": "xptoxpto","IO_16": "","IO_13": "13|relay_3|NORMAL","IO_00": "00|relay_2|NORMAL","IO_02": "02|relay_1|NORMAL","IO_15": ""},{"firmwareVersion":1.40}];
+            console.log('Config: ', response);
+
             $('input[name="nodeId"]').val(response[0].nodeId);
             $('input[name="directionCurrentDetection"]').prop("checked", response[0].directionCurrentDetection);
             $('select[name="notificationInterval"] option[value="' + response[0].notificationInterval + '"]').attr("selected", "selected");
@@ -43,9 +46,9 @@ function loadConfig() {
             $('select[name="IO_13"] option[value="' + response[0].IO_13 + '"]').attr("selected", "selected");
             $('select[name="IO_15"] option[value="' + response[0].IO_15 + '"]').attr("selected", "selected");
             $('select[name="IO_16"] option[value="' + response[0].IO_16 + '"]').attr("selected", "selected");
-        },
+        /*},
         timeout: 2000
-    });
+    });*/
 }
 
 function toggleActive(menu) {
@@ -55,11 +58,14 @@ function toggleActive(menu) {
 
 function loadReadings() {
     var someUrl = config.baseUrl + "/readings";
-    $.ajax({
+    /*$.ajax({
         url: someUrl,
         contentType: "text/plain; charset=utf-8",
         dataType: "json",
-        success: function (response) {
+        success: function (response) {*/
+            var response = {"temp_28ffe759321801dd":34.56,"voltagem":235.50,"amperagem":0.05,"potencia":13.00,"contador":0.17,"config":1.40};
+            console.log('Readings: ', response);
+
             if($('#sensors .GaugeMeter').length === 0){
                 Object.keys(response).reverse().forEach(function (key) {
                     if (key !== "config" ) {
@@ -74,9 +80,9 @@ function loadReadings() {
                     }
                 });
             }
-        },
+        /*},
         timeout: 2000
-    });
+    });*/
 }
 
 $(document).ready(function () {
